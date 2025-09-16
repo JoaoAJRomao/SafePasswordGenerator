01406ce29f358315943faaf0e96ef58813f5a810
João Romão <tupi_1800@hotmail.com>
2025-09-15 23:32:05 -0300

    Separate css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,22 +13,17 @@ export default function Home() {
   const [numbers, setNumbers] = React.useState(true);
   const [symbols, setSymbols] = React.useState(true);
 
+
+
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        minHeight: "100vh"
-      }}
-    >
-      Generate a password by clicking the button below
-      <Card style={{ width: 300 }}>
-        <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+    <div style={containerStyle} >
+      <h2 style={titleStyle}>Generate a password by clicking the button</h2>
+
+      <Card style={pwdCardStyle}>
+        <div style={pwdStyle}>
           <p>{pwd}</p>
           <CopyOutlined
-            style={{ fontSize: 20, cursor: "pointer", marginLeft: 8 }}
+            style={iconStyle}
             onClick={() => {
               navigator.clipboard.writeText(pwd);
             }}
@@ -36,8 +31,10 @@ export default function Home() {
           />
         </div>
       </Card>
+
       <Button
         type="primary"
+        style={btnGenerateStyle}
         onClick={() => {
           if (!capital && !lowercase && !numbers && !symbols) {
             setPwd("Please select at least one option");
@@ -56,17 +53,33 @@ export default function Home() {
         Generate password
       </Button>
       <div>
-        <Button onClick={() => setCapital(!capital)} style={{ backgroundColor: capital ? "green" : "red" }}>Include capital letters?</Button>
-        <Button onClick={() => setLowercase(!lowercase)} style={{ backgroundColor: lowercase ? "green" : "red" }}>Include lowercase letters?</Button>
+        <Button onClick={() => setCapital(!capital)} style={capital ? greenLight : redLight}>Include capital letters?</Button>
+        <Button onClick={() => setLowercase(!lowercase)} style={lowercase ? greenLight : redLight}>Include lowercase letters?</Button>
       </div>
-      <div>
-        <Button onClick={() => setNumbers(!numbers)} style={{ backgroundColor: numbers ? "green" : "red" }}>Include numbers?</Button>
-        <Button onClick={() => setSymbols(!symbols)} style={{ backgroundColor: symbols ? "green" : "red" }}>Include symbols?</Button>
+      <div style={{ marginTop: 10, marginBottom: 10 }}>
+        <Button onClick={() => setNumbers(!numbers)} style={numbers ? greenLight : redLight}>Include numbers?</Button>
+        <Button onClick={() => setSymbols(!symbols)} style={symbols ? greenLight : redLight}>Include symbols?</Button>
       </div>
-      <div className="margin-top">
+      <div className="margin-top" style={pwdLengthStyle}>
         Password length:
-        <Slider defaultValue={30} min={12} max={27} style={{ width: 200 }} onChange={setPwdLength} />
+        <Slider defaultValue={16} min={12} max={27} style={{ width: 200 }} onChange={setPwdLength} />
       </div>
     </div>
   );
 }
+
+const containerStyle: React.CSSProperties = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  minHeight: "100vh"
+};
+const titleStyle: React.CSSProperties = { marginBottom: 20 };
+const pwdCardStyle: React.CSSProperties = { width: 300, marginBottom: 10 };
+const pwdStyle: React.CSSProperties = { display: "flex", alignItems: "center", justifyContent: "space-between" };
+const btnGenerateStyle: React.CSSProperties = { marginBottom: 10 };
+const iconStyle: React.CSSProperties = { fontSize: 20, cursor: "pointer", marginLeft: 8 };
+const greenLight: React.CSSProperties = { backgroundColor: "green", borderColor: "green", color: "white" };
+const redLight: React.CSSProperties = { backgroundColor: "red", borderColor: "red", color: "white" };
+const pwdLengthStyle: React.CSSProperties = {border: "2px solid white" , borderRadius: 5, borderWidth: 1, padding: 10};
